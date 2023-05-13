1353a556f8da22c23f0243b56d601cdab893740a
tejas <pateltejas.2005@gmail.com>
2023-05-13 14:52:20 +0530

    first commit

--- frontend/src/components/GettingStarted.jsx
+++ frontend/src/components/GettingStarted.jsx
@@ -5,7 +5,7 @@ const GettingStarted = () => {
     <>
     <h2 className='text-xl'>About the Developer</h2>
     <div className='mt-2'>
-      <p>This Expense tracker application is developed by <span className='text-[#F57328]'>Tejas Patel</span> which tackles the daily problem of tracking day-to-day expenses.</p>
+      <p>This Expense tracker application is developed by <span className='text-[#F57328]'>Rashmit Tiwari</span> which tackles the daily problem of tracking day-to-day expenses.</p>
       <p className='mt-2'>You need to SignIn and then enjoy the application :) Hope you like it. </p>
     </div>
     </>
