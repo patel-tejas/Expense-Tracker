e87703a061b6c982521411cab0bcd1b0ffb3e8d6
Tejas Patel <115111286+patel-tejas@users.noreply.github.com>
2024-04-26 19:59:53 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
     <div className='flex flex-col sm:flex-row gap-1 text-white w-full items-center justify-center h-[10vh] text-[10px] sm:text[16px]'>
         <p>Copyright &#169; 2023</p>
-        <p>Made with <span style={{color: "red"}}>&hearts;</span> by TechTez</p>
+        <p>Made with <span style={{color: "red"}}>&hearts;</p>
     </div>
     </>
   )
